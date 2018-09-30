5302d05ae4c26b0928060f5a1ff7cc8ee97ba58e
Stefan Evans <stefan.evans@consegna.cloud>
2018-09-30 16:57:52 +1300

    .

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  api: 'https://api.bookinbot.com',
+  region: 'ap-southeast-2',
+  // cognito
+  userpoolid: 'ap-southeast-2_CHrxkLm0j',
+  clientid: '1ipivcqvud37c1dckr88qi29e6'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+
+export const environment = {
+  production: false,
+  api: 'http://localhost:3000',
+  region: 'ap-southeast-2',
+  // cognito
+  userpoolid: 'ap-southeast-2_CHrxkLm0j',
+  clientid: '1ipivcqvud37c1dckr88qi29e6'
+};
