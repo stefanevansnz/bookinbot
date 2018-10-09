9cf0f1a8e13569226824fb04cbee48e0f8f51d21
Stefan Evans <stefan.evans@consegna.cloud>
2018-10-09 21:33:31 +1300

    .

--- deploy_angular.sh
+++ deploy_angular.sh
@@ -1,6 +1,6 @@
 . ./settings.sh
 ng build --prod --aot --output-hashing all 
 cd serverless
-serverless client deploy --stage prod
+sls client deploy --stage prod
 cd ..
 # http://bookinbot.com-dist.s3-website-ap-southeast-2.amazonaws.com/signup
